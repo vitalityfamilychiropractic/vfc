e0acc3b949f397e8557b6c0fe98be6fe6f10036a
Nick McLarty <nmclarty@tamus.edu>
2022-07-30 16:53:40 -0500

    Add tooling to contact form

---
+++ functions/_middleware.ts
@@ -0,0 +1,15 @@
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: env.CONTACT_FORM_TO_NAME, email: env.CONTACT_FORM_TO_EMAIL }],
+    },
+  ],
+  from: { name: env.CONTACT_FORM_FROM_NAME, email: env.CONTACT_FORM_FROM_EMAIL },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/thank-you" },
+    }),
+});
