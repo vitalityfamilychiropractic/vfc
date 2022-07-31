2e69efc670000c592b693f7780182179bd847cb3
Nick McLarty <nmclarty@tamus.edu>
2022-07-30 21:30:30 -0500

    Add env attribute to middleware function

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,21 +1,23 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = (context) => mailChannelsPlugin({
-  personalizations: [
-    {
-      to: [{
-        name: context.env.CONTACT_FORM_TO_NAME,
-        email: context.env.CONTACT_FORM_TO_EMAIL,
-      }],
+export const onRequest = (context) => {
+  return mailChannelsPlugin({
+    personalizations: [
+      {
+        to: [{
+          name: context.env.CONTACT_FORM_TO_NAME,
+          email: context.env.CONTACT_FORM_TO_EMAIL,
+        }],
+      },
+    ],
+    from: {
+      name: context.env.CONTACT_FORM_FROM_NAME,
+      email: context.env.CONTACT_FORM_FROM_EMAIL,
     },
-  ],
-  from: {
-    name: context.env.CONTACT_FORM_FROM_NAME,
-    email: context.env.CONTACT_FORM_FROM_EMAIL,
-  },
-  respondWith: () => {
-    return new Response(
-      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
-    );
-  },
-});
+    respondWith: () => {
+      return new Response(
+        `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+      );
+    },
+  })(context);
+};
