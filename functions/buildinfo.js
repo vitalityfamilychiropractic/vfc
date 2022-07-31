adc41d40a7b561641cb629801c497a99dadcd94b
Nick McLarty <nmclarty@tamus.edu>
2022-07-31 14:43:03 -0500

    Add verbose debugging to buildinfo

--- functions/buildinfo.js
+++ functions/buildinfo.js
@@ -1,3 +1,4 @@
 export async function onRequest(context) {
-  return new Response(context.env.CF_PAGES_BRANCH + ' ' + context.env.CF_PAGES_COMMIT_SHA);
+  return new Response(JSON.stringify(context));
+//  return new Response(context.env.CF_PAGES_BRANCH + ' ' + context.env.CF_PAGES_COMMIT_SHA);
 }
