095b38fc26bff7c2784c399f6928cd11419203b0
Nick McLarty <nmclarty@tamus.edu>
2022-07-30 19:36:30 -0500

    Add env attribute to middleware function

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,15 +1,19 @@
 import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = mailchannelsPlugin({
-  personalizations: [
-    {
-      to: [{ name: CONTACT_FORM_TO_NAME, email: CONTACT_FORM_TO_EMAIL }],
-    },
-  ],
-  from: { name: CONTACT_FORM_FROM_NAME, email: CONTACT_FORM_FROM_EMAIL },
-  respondWith: () =>
-    new Response(null, {
-      status: 302,
-      headers: { Location: "/thank-you" },
-    }),
-});
+export async function onRequest({ env }) {
+  const task = await mailchannelsPlugin({
+    personalizations: [
+      {
+        to: [{ name: env.CONTACT_FORM_TO_NAME, email: env.CONTACT_FORM_TO_EMAIL }],
+      },
+    ],
+    from: { name: env.CONTACT_FORM_FROM_NAME, email: env.CONTACT_FORM_FROM_EMAIL },
+    respondWith: () =>
+      new Response(null, {
+        status: 302,
+        headers: { Location: "/thank-you" },
+      }),
+  });
+
+  return new Response(task);
+}
